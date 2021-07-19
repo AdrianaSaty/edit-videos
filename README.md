ee676cf730fd91d9ea06b1ce1c1a602bbe3548a2
Adriana Saty <adri-saty@hotmail.com>
2021-07-18 21:08:56 -0300

    deleta esse commit

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 Convert a video file to an animated GIF with Web Assembly. 
 
+### if you want to test it online, try it:
+https://adrianasaty.github.io/edit-videos/
+
+
 ### npm start
 
 Runs the app in the development mode.
 Open http://localhost:8080 to view it in the browser.
-
--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ const ffmpeg = createFFmpeg({ log: true });
 function App() {
   const [ready, setReady] = useState(false);
   const [importedVideo, setVideo] = useState();
+  const [cutedVideo, setCutedVideo] = useState();
+  const [logoVideo, setLogoVideo] = useState();
   const [finalVideo, setFinalVideo] = useState();
 
   const load = async () => {
@@ -19,8 +21,10 @@ function App() {
   }, [])
 
   const cutVideo = async () => {
+    console.log('-----> CUT VIDEO')
+    console.log(importedVideo)
     // Write the file to memory 
-    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(importedVideo));
+    ffmpeg.FS('writeFile', 'importedVideo.mp4', await fetchFile(importedVideo));
 
     // Run the FFMpeg command
     // -i: import
@@ -29,44 +33,53 @@ function App() {
     // -f: format
 
     await ffmpeg.run(
-      '-i', 'test.mp4', 
-      '-t', '2.5', 
-      '-ss', '10.0', 
-      '-f', 'mp4', 'out.mp4');
+      '-i', 'importedVideo.mp4',
+      '-t', '2.5',
+      '-ss', '10.0',
+      '-f', 'mp4',
+      'cutedVideo.mp4'
+    );
+    const data = ffmpeg.FS('readFile', 'cutedVideo.mp4');
+    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+    setCutedVideo(url);
+    setFinalVideo(url);
+    await putLogoVideo();
+  }
 
-    // Read the result
-    const data = ffmpeg.FS('readFile', 'out.mp4');
+  const putLogoVideo = async () => {
+    console.log('-----> PUT LOGO VIDEO')
+    console.log(cutedVideo);
 
-    // Create a URL
-    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4'}));
-    setFinalVideo(url)
+    await ffmpeg.FS('writeFile', 'cutedVideo.mp4', fetchFile(cutedVideo));
+
+    await ffmpeg.run(
+      '-i', 'cutedVideo.mp4',
+      '--filter_complex', "[0:v]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1,setsar=1,fps=30000/1001,format=yuv420p[intro];[1:v]scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1,setsar=1,fps=30000/1001,format=yuv420p[video];[2]scale=250:-1[logo];[0:a]aformat=sample_rates=48000:channel_layouts=stereo[introa];[1:a]aformat=sample_rates=48000:channel_layouts=stereo[videoa];[intro][introa][video][videoa]concat=n=2:v=1:a=1[vid][a];[vid][logo]overlay=W-w-200:H-h-700[v]",
+      'cutedVideo.mp4'
+    );
+    const data = ffmpeg.FS('readFile', 'cutedVideo.mp4');
+    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+    setLogoVideo(url);
+    setFinalVideo(url);
   }
 
   return ready ? (
-    
     <div className="App">
-      { importedVideo && <video
+      {importedVideo && <video
         controls
         width="400"
         src={URL.createObjectURL(importedVideo)}>
-
       </video>}
-
-
       <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
-
       <h3>Result</h3>
-
       <button onClick={cutVideo}>Faz a braba! </button>
-
-      { finalVideo && 
-      <>
-      <video controls width="400" src={finalVideo} />
-      <a href={finalVideo} download>
-        Download!
-    </a>
-      </>}
-
+      {finalVideo &&
+        <>
+          <video controls width="400" src={finalVideo} />
+          <a href={finalVideo} download>
+            Download!
+          </a>
+        </>}
     </div>
   )
     :
